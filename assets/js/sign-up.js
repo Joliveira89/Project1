4067ee30ef40cc261a2fa80f844d7b6fe0cf0f3b
gbmahili <georgesmahili@gmail.com>
2017-12-31 09:52:18 +0100

    Added firebase script to the index.html page and initialized the firebase app in the in sign-up.js file

--- assets/js/sign-up.js
+++ assets/js/sign-up.js
@@ -5,7 +5,25 @@ $(document).ready(function () {
     // userName = $("#userName").val().trim();
     // userPassword = $("#userPassword").val().trim();
     // dob = $("#dob").val().trim();
+    //
 
-    age = parseInt(moment("05/03/1982", "MM/DD/YYYY").month(0).from(moment().month(0)).split(" ")[0]);
-    console.log(typeof age);
+    dob = "05/03/2000";//This value will be taken from the input
+    age = parseInt(moment(dob, "MM/DD/YYYY").month(0).from(moment().month(0)).split(" ")[0]);
+    console.log(age);
+
+    // Object to store the user's info for the Firebase
+
+    var userProfile = {};
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyD6nYJpi1UtXV7JEzgAY5tiL6cNptngPZk",
+        authDomain: "freebay-dbbdc.firebaseapp.com",
+        databaseURL: "https://freebay-dbbdc.firebaseio.com",
+        projectId: "freebay-dbbdc",
+        storageBucket: "freebay-dbbdc.appspot.com",
+        messagingSenderId: "328886904095"
+    };
+    firebase.initializeApp(config);
+
 });
