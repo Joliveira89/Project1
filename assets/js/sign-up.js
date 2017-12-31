297298755b76823e9c86399543536791cb1cd01e
gbmahili <georgesmahili@gmail.com>
2017-12-31 11:04:30 +0100

    Added firebase script to push and pull data from the database. While pulling data from the database, we will check if user is over 18 years old

--- assets/js/sign-up.js
+++ assets/js/sign-up.js
@@ -2,19 +2,6 @@ $(document).ready(function () {
     // alert('foo');
     var userName, dob, age, userEmail, userPassword;
 
-    // userName = $("#userName").val().trim();
-    // userPassword = $("#userPassword").val().trim();
-    // dob = $("#dob").val().trim();
-    //
-
-    dob = "05/03/2000";//This value will be taken from the input
-    age = parseInt(moment(dob, "MM/DD/YYYY").month(0).from(moment().month(0)).split(" ")[0]);
-    console.log(age);
-
-    // Object to store the user's info for the Firebase
-
-    var userProfile = {};
-
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyD6nYJpi1UtXV7JEzgAY5tiL6cNptngPZk",
@@ -25,5 +12,49 @@ $(document).ready(function () {
         messagingSenderId: "328886904095"
     };
     firebase.initializeApp(config);
+    // Create a database
+    var database = firebase.database();
+
+    // When a user clicks on 'Sign UP', Add user to the database. All users can sign up
+    $("#sign-up").click(function (e) {//TODO: Find out from Veena what she called the SignUp button:
+        e.preventDefault();
+
+        // Get data from the user:
+        userName = $("#userName").val().trim();
+        // userName = "gbmahili";
+        userEmail = $("#userEmail").val().trim();
+        // userEmail = "georgesmahili@gmail.com";
+        userPassword = $("#userPassword").val().trim();
+        // userPassword = "test";
+        userDOB = $("#userDOB").val().trim();
+        // userDOB = "05/03/2000";//This value will be taken from the input
+        
+        // Object to store the user's info for the Firebase:
+        var userProfile = {
+            userName : userName,
+            userPassword : userPassword,
+            userEmail : userEmail,
+            userDOB : userDOB
+        };
+        //Push collected information to the database:
+        database.ref().push(userProfile);
+        //we are emptying the input values 
+        $(".form-control").val("");
+        alert('foo');
+    });
+
+    //TO DO:
+    // When working on the sign-in script, check the user's age.
+    //if they are under 18, restrict them from R-rated movies after they sign in
+    database.ref().on("child_added", function (childSnapshot) {
+
+        //DB Data
+        var usersData = childSnapshot.val();
+        console.log(usersData);
+        // Calculate age based on date of birth provided by the user: 
+        userAge = parseInt(moment(usersData.userDOB, "MM/DD/YYYY").month(0).from(moment().month(0)).split(" ")[0]);
+        console.log(userAge);
+    });
+    
 
 });
