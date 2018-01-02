c238aec7747f652b43b9e63f9c0139a1a77c88d6
gbmahili <georgesmahili@gmail.com>
2018-01-01 20:54:40 -0500

    added logic to show/not show movies based on age. Logic added to  app2.js

--- app2.js
+++ app2.js
@@ -1,12 +1,14 @@
 var omdbKey = "f2ddb033";
 
 function returnMovie() {
+  var currentUserAge = parseInt($("#current-user-age").attr("current-user-age"));
 
   var searchTerm = $("#userinputsearch").val().trim();
   var queryURLBase = "https://www.omdbapi.com/?t=" + searchTerm + "&apikey=" + omdbKey;
 
-  $("#postersection").empty();
+  
   $("#movieinfosection").empty();  
+  $("#postersection").empty();
 
   $.ajax({
     url: queryURLBase,
@@ -19,13 +21,38 @@ function returnMovie() {
     var pTwo = $("<p>").text("Actors: " + actors);
     var runtime = response.Runtime;
     var pThree = $("<p>").text("Runtime: " + runtime);
-    var rating = response.Rated;
+    rating = response.Rated;
     var pFour = $("<p>").text("Rating: " + rating);
-    $("#postersection").append(pOne);
-    $("#movieinfosection").append(pTwo);
-    $("#movieinfosection").append(pThree);
-    $("#movieinfosection").append(pFour);
-  })
+
+    //Run the checkAgeFirst()
+    //checkAgeFirst();
+    //1. First, we check if the movie is not rated R
+    if (rating != "R"){
+      //If not, we show everything      
+      $("#postersection").append(pOne);
+      $("#movieinfosection").empty();
+      $("#movieinfosection").append(pTwo);
+      $("#movieinfosection").append(pThree);
+      $("#movieinfosection").append(pFour);
+    }else{
+      //2. If it is rated R, we check the age of the user. If the user is 18 or older:
+      if (currentUserAge >= 18) {
+        //We show everything else
+        $("#postersection").append(pOne);
+        $("#movieinfosection").empty();
+        $("#movieinfosection").append(pTwo);
+        $("#movieinfosection").append(pThree);
+        $("#movieinfosection").append(pFour);
+      }else{
+        //Else, we show a modal: TODO//Create a modal and replace with console.log
+        console.log("You are younger than 18, we can't show you the result as the movie is rated R.");
+        $("#movieinfosection").empty();
+        $("#movieinfosection").css("background", "#fafafa").html("<h1>You are younger than 18, we can't show you the result as the movie is rated R.");
+        $("#postersection").empty();
+      }      
+    }
+    
+  });
 
 
   var queryURLYoutube = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyC45ynEdLhjV2bjYjpFRLPA2vtD89f3m80&maxResults=1&q=" + searchTerm + " trailer";
@@ -36,7 +63,23 @@ function returnMovie() {
   }).done(function(response) {
     var video = "https://www.youtube.com/embed/" + response.items[0].id.videoId;
     var pFive = $("<iframe>").attr("src", video);
-    $("#postersection").append(pFive);
+    //1. First, we check if the movie is not rated R
+    if (rating != "R") {
+      //If not, we show everything  
+      $("#postersection").append(pFive);
+    } else {
+      //2. If it is rated R, we check the age of the user. If the user is 18 or older:
+      if (currentUserAge >= 18) {
+        //We show everything else
+        $("#postersection").append(pFive);
+      } else {
+        //Else, we show a modal: TODO//Create a modal and replace with console
+        console.log("You are younger than 18, we can't show you the result as the movie is rated R.");
+        $("#movieinfosection").empty();
+        $("#movieinfosection").css("background", "#fafafa").html("<h1>You are younger than 18, we can't show you the result as the movie is rated R.");
+        $("#postersection").empty();
+      }
+    }   
     console.log(response);
     console.log(video);
   })
@@ -46,4 +89,4 @@ $(document).on("click", ".input-group-addon", returnMovie);
 
 // youtube key = AIzaSyC45ynEdLhjV2bjYjpFRLPA2vtD89f3m80
 
-//Testing git push by Baraka.
+//Testing Area for Baraka
